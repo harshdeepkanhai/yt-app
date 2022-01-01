22eb7bccceb82064b61b87555cdedf841eea5009
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2022-01-01 23:57:49 +0530

    Searching for Videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyCRVSopKerqcjKIRamyJF5VEYG6ogTsYe4'
+
+export default axios.create({
+    baseURL: ' https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+})

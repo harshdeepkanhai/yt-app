be912274b2604b71155497bc1f88708aaded255c
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2022-01-02 11:54:54 +0530

    Rendering Video Thumbnails

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const VideoItem = props => {
+const VideoItem = ({ video }) => {
     return (
         <div>
-            Video Item
+            <img src={video.snippet.thumbnails.medium.url} />
+            {video.snippet.title}
         </div>
     )
 }
